47d912a720a19d49def15bf761a6d0d134dd0f78
Lukas Finkbeiner <lfinkbei@mpe.mpg.de>
2023-08-03 13:46:52 +0200

    Fixed a bizarre bug in quick_run_H that seemed to use a write label as though it were building a massless-neutrino emulator, which of course violates the new practice that massive-neutrino emulators be denoted with capital letters. What's bizarre is that, though the script clearly reads incorrectly, the results were written correctly to the disk on Tuesday. That probably means that we ran an older version of the script.

--- cassL/quick_run_h.py
+++ cassL/quick_run_h.py
@@ -4,14 +4,25 @@ from cassL import user_interface as ui
 
 massive_neutrinos=False
 
+emu_stage = "test"
+# Technically, this variable is not very user-friendly anymore, because we often
+# use hypercubes from older versions...
+emu_vlabel = "h1"
+
+file_suffix = emu_vlabel + "_" + emu_stage
+
 standard_k = np.load("current_data_files/standard_k.npy", allow_pickle=True)
-hc_sigma12_pred = np.load("../best_lhc_unit_G8_massless.npy", allow_pickle=True)
+hc = np.load("../best_lhc_unit_" + file_suffix + ".npy",
+    allow_pickle=True)
 param_ranges = ui.get_param_ranges(massive_neutrinos=massive_neutrinos)
 # del param_ranges["sigma12"]
 # param_ranges["sigma12_2"] = [0.04, 1]
-samples_H2, rescalers_H2 = ged.fill_hypercube(
-    hc_sigma12_pred, standard_k, param_ranges,
+samples, rescalers = ged.fill_hypercube(
+    hc, standard_k, param_ranges,
     massive_neutrinos=massive_neutrinos, write_period=100,
-    save_label="h1_unit_train"
+    save_label="unit_" + file_suffix
 )
+np.save("samples_unit_" + file_suffix + ".npy", samples, allow_pickle=True)
+np.save("rescalers_unit_" + file_suffix + ".npy", rescalers, allow_pickle=True)
+np.save("lhc_unit_" + file_suffix + ".npy", hc, allow_pickle=True)
 
