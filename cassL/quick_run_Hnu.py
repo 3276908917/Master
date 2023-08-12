b2b03ceb080c38ac120d70508acf993e3e264808
Lukas Finkbeiner @ Otto von Bismarck <lfinkbeiner@berkeley.edu>
2023-08-12 14:33:34 +0200

    Small expansions in the outline
    We've also introduced some fairly useful code in the quick run script for picking up from where we left off. I hope we can include that in the other script eventually, too.
    
    Also in this commit: a clear roadmap, all of the emulators we need to collect on a flashdrive or something so that we're not spinning our wheels when we switch platforms.

--- cassL/quick_run_Hnu.py
+++ cassL/quick_run_Hnu.py
@@ -4,11 +4,11 @@ from cassL import user_interface as ui
 
 massive_neutrinos=True
 
-emu_stage = "test"
+emu_stage = "train"
 # Technically, this variable is not very user-friendly anymore, because we often
 # use hypercubes from older versions...
 emu_vlabel = "Hnu2direct"
-hc_vlabel = "Hnu2"
+hc_vlabel = "10k_massive"
 
 file_suffix = emu_vlabel + "_" + emu_stage
 
@@ -19,8 +19,17 @@ param_ranges = ui.get_param_ranges(priors="COMET",
     massive_neutrinos=massive_neutrinos)
 # del param_ranges["sigma12"]
 #param_ranges["sigma12_root"] = [0.4472135954999579, 1]
+
+completed_index = 3999
+samples = None
+
+if completed_index is not None:
+    file_ending = str(completed_index) + "_unit_" + file_suffix + ".npy"
+    samples = np.load("samples_backup_i" + file_ending, allow_pickle=True)
+    hc = np.load("hc_backup_i" + file_ending, allow_pickle=True)
+
 samples, rescalers = ged.fill_hypercube(
-    hc, standard_k, param_ranges,
+    hc, standard_k, param_ranges, samples=samples,
     massive_neutrinos=massive_neutrinos, write_period=500,
     save_label="unit_" + file_suffix
 )
