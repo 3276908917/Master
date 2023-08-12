7c2a7a163ce0b390a97c00cf35f70faf8933df57
Lukas Finkbeiner @ Otto von Bismarck <lfinkbeiner@berkeley.edu>
2023-08-13 01:06:35 +0200

    Protect file handling in quick run
    Now the completed_index variable is -1 instead of None, meaning it is more versatile. Furthermore, if completed_index is not -1 but we don't have a relevant backup, we reset it to -1.
    
    This quick-run script has become quite involved, so  we now have even greater pressure to develop this train_emu script.

--- cassL/quick_run_Hnu.py
+++ cassL/quick_run_Hnu.py
@@ -8,7 +8,7 @@ emu_stage = "train"
 # Technically, this variable is not very user-friendly anymore, because we often
 # use hypercubes from older versions...
 emu_vlabel = "Hnu2direct"
-hc_vlabel = "10k_massive"
+hc_vlabel = "7k_massive"
 
 file_suffix = emu_vlabel + "_" + emu_stage
 
@@ -20,21 +20,31 @@ param_ranges = ui.get_param_ranges(priors="COMET",
 # del param_ranges["sigma12"]
 #param_ranges["sigma12_root"] = [0.4472135954999579, 1]
 
-completed_index = 3999
+completed_index = -1
 samples = None
 
-if completed_index is not None:
-    file_ending = str(completed_index) + "_unit_" + file_suffix + ".npy"
-    samples = np.load("samples_backup_i" + file_ending, allow_pickle=True)
-    hc = np.load("hc_backup_i" + file_ending, allow_pickle=True)
+if completed_index > -1:
+    try:
+        file_ending = str(completed_index) + "_unit_" + file_suffix + ".npy"
+        samples = np.load("samples_backup_i" + file_ending, allow_pickle=True)
+        hc = np.load("hc_backup_i" + file_ending, allow_pickle=True)
+    except FileNotFoundError:
+        # Probably what happened is that we just didn't reach the end of the
+        # first write interval. So, we have to start from the beginning.
+        completed_index = -1
 
 samples, rescalers = ged.fill_hypercube(
     hc, standard_k, param_ranges, samples=samples,
     massive_neutrinos=massive_neutrinos, write_period=500,
-    cell_range=range(completed_index + 1, 5000),
+    cell_range=range(completed_index + 1, len(hc)),
     save_label="unit_" + file_suffix
 )
 np.save("samples_unit_" + file_suffix + ".npy", samples, allow_pickle=True)
 np.save("rescalers_unit_" + file_suffix + ".npy", rescalers, allow_pickle=True)
 np.save("lhc_unit_" + file_suffix + ".npy", hc, allow_pickle=True)
 
+### Wouldn't it be cool if this thing automatically cleaned up backups that
+### aren't needed anymore??
+### Like, we could maintain a last_backup_handle label. Every time we complete a
+### new backup, we delete the file located at last_backup_handle and update the
+### label to the new backup.
