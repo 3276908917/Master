9bca2c09f03f2cc05a53ac8a8ba0077a1b2216b5
Lukas Finkbeiner <lfinkbei@mpe.mpg.de>
2023-08-29 14:01:29 +0200

    Add new comparators feature to the LHC generator, which allows the user some degree of control over what is considered an optimal LHC.

--- cassL/lhc.py
+++ cassL/lhc.py
@@ -15,18 +15,19 @@ def generate_samples(ranges, n_samples, n_trials=0, validation=False):
     r"""
     Generate samples on a latin hypercube
 
+    This is the function that served us well for a long time. It should be the
+    last outdated function to be deleted.
+
     Parameters
     ----------
     ranges : collections:OrderedDict
-        an ordered dictionary, the order
-        of the outputs in latin hypercube
-        is set by this order
+        an ordered dictionary, the order of the outputs in latin hypercube is
+        set by this order
     n_samples : int
         The number of samples to generate
     n_trials : int
-        Regenerate cube this number of times
-        to find the cube that maximises the
-        minimum distance between samples
+        Regenerate cube this number of times to find the cube that maximises
+        the minimum distance between samples
     validation : bool (Optional)
         Generate random verification data
 
@@ -35,7 +36,7 @@ def generate_samples(ranges, n_samples, n_trials=0, validation=False):
     samples : 2d array
         the latin hypercube
     """
-    
+
     '''
     Lukas:
     Did you mean to say, "if not"?
@@ -94,10 +95,21 @@ def generate_samples(ranges, n_samples, n_trials=0, validation=False):
     return samples, np.array(list_min_dist)
 
 def long_term_LHC_builder(param_ranges, n_samples, label="unlabeled"):
-    n_params = len(param_ranges)
+    n_params = len(param_ranges, comparator=np.greater)
     """
     This is like multithread_LHC_builder but not multithreaded...
-    We should really collapse these functions...    
+    We should really collapse these functions...
+
+    Parameters
+    ----------
+    @comparator
+        To see if hypercube A is better than hypercube B, we use the if
+        statement
+        if comparator(A's minimum separation, B's minimum separation)
+        That is to say, if this if-condition evaluates to True, we consider A
+        as better than B.
+        The two obvious options are np.greater and np.less. np.greater is the
+        default and should be used if you are unsure.
     """
 
     '''
@@ -122,11 +134,12 @@ def long_term_LHC_builder(param_ranges, n_samples, label="unlabeled"):
         # If the new minimum distance is larger than the one we stored,
         # then we pick this configuration and save it, broadcasting the
         # update
-        if (min_dist_new > min_dist):
+        if (comparator(min_dist_new, min_dist)):
             min_dist = min_dist_new
             samples = samples_new
-            print("New best found after", i, "trials. New minimum distance is",
-                str(min_dist_new) + ".", "Saving...")
+            print("New best (" + str(min_dist_new) + ") found after", i,
+                  "trials. New minimum distance is " + str(min_dist_new) + \
+                  ". Saving...")
 
             '''
             The lhs function returns a LHC normalized within the range [0,1]
@@ -142,7 +155,19 @@ def long_term_LHC_builder(param_ranges, n_samples, label="unlabeled"):
 
 import concurrent.futures
 
-def batch(n_params, n_samples=5000):
+def batch(n_params, n_samples=5000, comparator=np.greater):
+    """
+    Parameters
+    ----------
+    @comparator
+        To see if hypercube A is better than hypercube B, we use the if
+        statement
+        if comparator(A's minimum separation, B's minimum separation)
+        That is to say, if this if-condition evaluates to True, we consider A
+        as better than B.
+        The two obvious options are np.greater and np.less. np.greater is the
+        default and should be used if you are unsure.
+    """
     samples = lhs(n=n_params, samples=n_samples, criterion='center')
     # We want to get a good configuration for the LHC, that maximises
     # the minimum distance among points.
@@ -156,14 +181,14 @@ def batch(n_params, n_samples=5000):
         # If the new minimum distance is larger than the one we stored,
         # then we pick this configuration and save it, broadcasting the
         # update
-        if (min_dist_new > min_dist):
+        if comparator(min_dist_new, min_dist):
             min_dist = min_dist_new
             samples = samples_new
 
     return samples, min_dist
 
 def multithread_LHC_builder(param_ranges, n_samples, label="unlabeled",
-    previous_record=0):
+    previous_record=0, comparator=np.greater):
     """
     Use more CPU to compute more random LHCs, periodically saving the one with
     the greatest minimum separation. In other words, our LHC generation
@@ -174,6 +199,17 @@ def multithread_LHC_builder(param_ranges, n_samples, label="unlabeled",
     * COMET priors
         * massive case: 0.0802202670064637
             (saved under "best_lhc_multi2.npy")
+
+    Parameters
+    ----------
+    @comparator
+        To see if hypercube A is better than hypercube B, we use the if
+        statement:
+        if comparator(A's minimum separation, B's minimum separation)
+        That is to say, if this if-condition evaluates to True, we consider A
+        as better than B.
+        The two obvious options are np.greater and np.less. np.greater is the
+        default and should be used if you are unsure.
     """
     n_params = len(param_ranges)
     total_num_cubes = 0
@@ -198,18 +234,19 @@ def multithread_LHC_builder(param_ranges, n_samples, label="unlabeled",
 
         for future in futures:
             this_lhc, this_min_dist = future.result()
-            if this_min_dist > overall_min_dist:
+            if comparator(this_min_dist, overall_min_dist):
                 overall_min_dist = this_min_dist
                 overall_best_lhc = this_lhc
 
-                for n, (par, param_range) in enumerate(iteritems(param_ranges)):
+                for n, (par, param_range) in \
+                    enumerate(iteritems(param_ranges)):
                     overall_best_lhc[:, n] = overall_best_lhc[:, n] * \
                     (param_range[1] - param_range[0]) + param_range[0]
                 np.save("best_lhc_" + label + ".npy", overall_best_lhc,
                     allow_pickle=True)
 
-                print("New best found! New minimum distance is",
-                    str(overall_min_dist) + ".", "Saving...")
+                print("New best (" + str(overall_min_dist) + \
+                      ") found! Saving...")
 
         #outs = concurrent.futures.wait(futures)
 
@@ -220,7 +257,7 @@ def multithread_LHC_builder(param_ranges, n_samples, label="unlabeled",
         '''
 
 def multithread_unit_LHC_builder(dim, n_samples, label="unlabeled",
-    num_workers=12, previous_record=0):
+    num_workers=12, previous_record=0, comparator=np.greater):
     """
     Use more CPU to compute more random LHCs, periodically saving the one with
     the greatest minimum separation. In other words, our LHC generation
@@ -231,13 +268,19 @@ def multithread_unit_LHC_builder(dim, n_samples, label="unlabeled",
     * COMET priors
         * massive case: 0.0802202670064637
             (saved under "best_lhc_multi2.npy")
+
+    Parameters
+    ----------
+    @comparator
+        To see if hypercube A is better than hypercube B, we use the if
+        statement
+        if comparator(A's minimum separation, B's minimum separation)
+        That is to say, if this if-condition evaluates to True, we consider A
+        as better than B.
+        The two obvious options are np.greater and np.less. np.greater is the
+        default and should be used if you are unsure.
     """
 
-    # This is the external function, 'lhs.' It needs to know the size
-    # of the parameter space, the number of points you want, and the
-    # criterion to select points. 'center' means that it first divides
-    # each dimension of the parameter space into n_samples equispaced
-    # intervals and it picks the middle point of the chosen interval
     overall_min_dist = previous_record
     overall_best_lhc = None
 
@@ -250,20 +293,27 @@ def multithread_unit_LHC_builder(dim, n_samples, label="unlabeled",
 
         for future in futures:
             this_lhc, this_min_dist = future.result()
-            if this_min_dist > overall_min_dist:
+            if comparator(this_min_dist, overall_min_dist):
                 overall_min_dist = this_min_dist
                 overall_best_lhc = this_lhc
 
                 np.save("best_lhc_" + label + ".npy", overall_best_lhc,
                     allow_pickle=True)
 
-                print("New best found! New minimum distance is",
-                    str(overall_min_dist) + ".", "Saving...")
+                print("New best (" + str(overall_min_dist) + \
+                      ") found! Saving...")
 
-        #outs = concurrent.futures.wait(futures)
 
-        '''
-        The lhs function returns a LHC normalized within the range [0,1]
-        for each dimension. We have to rescale the LHC to match the range
-        we want for our cosmological parameters
-        '''
+def nearness_comparator(target):
+    """
+    This function returns a Boolean comparator function based on numerical
+    proximity.
+
+    The comparator works like this: comparator(a, b) returns True if a is
+    closer to target than b, otherwise False.
+
+    #! Should we implement error checking here?
+    """
+    comparator = lambda a, b: np.abs(a - target) < np.abs(b - target):
+    return comparator
+
