699b368a9be8b2433f5a5127f73e7012633c6bb9
3276908917 @ MPELaptop <microwavecow@gmail.com>
2023-11-15 12:39:19 +0100

    Introduce small sigma12 test script
    I really should have committed this yesterday at work, so we recreated this script.
    
    We'll delete this later once the code has undergone basic tests of accuracy.

---
+++ sigma12_emu.py
@@ -0,0 +1,20 @@
+from cassL import generate_emu_data as ged
+from cassL import user_interface as ui
+from cassL import camb_interface as ci
+import numpy as np
+
+priors = ui.prior_file_to_array("COMET")
+
+def eval_func(cosmology):
+    return ci.evaluate_sigma12(cosmology, [1])
+
+sigma12_train_lhs = np.load("lhc_sigma12_train.npy")
+
+sigma12_train_samples, _ = ged.fill_hypercube(sigma12_train_lhs, None, priors,
+    eval_func, save_label = "sigma12_train")
+    
+sigma12_test_lhs = np.load("lhc_sigma12_test.npy")
+
+sigma12_test_samples, _ = ged.fill_hypercube(sigma12_test_lhs, None, priors,
+    eval_func, save_label = "sigma12_test")
+    
