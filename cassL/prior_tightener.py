97db4f05f8aa33439b9fcb664ccb895f6c4cce4c
Lukas Finkbeiner @ Otto von Bismarck <lfinkbeiner@berkeley.edu>
2023-12-30 18:43:11 +0100

    Critical prior_tightener bug fixed
    We were re-using the hc label even though that cosmology's sigma12 field was getting mutated by the fill_hypercube calls!!
    
    With this fix, the results of prior_tightener remain valid when plugged into quickrun_h_haggle.
    
    Two commits ago, we reported that the new conservatism is 39%. However, this number was erroneously low because of the prior_tightener bug addressed in this commit. We've re-run the script and determined that the correct conservatism is 45%!

--- cassL/prior_tightener.py
+++ cassL/prior_tightener.py
@@ -3,6 +3,8 @@ import numpy as np
 from cassL import generate_emu_data as ged
 from cassL import user_interface as ui
 
+import copy as cp
+
 # If a particular range of cells is already known to cause problems, it may be
 # more efficient to change this value away from None.
 special_cell_range = range(3247, 3250)
@@ -10,20 +12,20 @@ special_cell_range = range(3247, 3250)
 standard_k = np.load("data_sets/k/300k.npy", allow_pickle=True)
 
 #hc = np.load("lhc_prior_tightener.npy", allow_pickle=True)
-hc = np.load("data_sets/Hnu3/lhc_val_initial.npy")
+hc = np.load("data_sets/Hnu3/lhc_val_initial.npy", allow_pickle=True)
 
 safe = ui.prior_file_to_array("COMET")
 dangerous = ui.prior_file_to_array("CLASSIC")
 
-conservatism = 0.5
-
 # implement a binary search approach
-
 highest_failed_conservatism = 0.0
 lowest_successful_conservatism = 1.0
 
-# Keep testing values of conservatism until the discrepancy reaches percent
-while lowest_successful_conservatism - highest_failed_conservatism >= 0.01:
+priors = lambda c: c * safe + (1 - c) * dangerous
+
+# Keep testing values of conservatism until the discrepancy reaches the percent
+# level.
+while lowest_successful_conservatism - highest_failed_conservatism > 0.01:
     average_bound = (highest_failed_conservatism + \
                      lowest_successful_conservatism) / 2
     conservatism = np.around(average_bound, 2)
@@ -34,11 +36,14 @@ while lowest_successful_conservatism - highest_failed_conservatism >= 0.01:
                           "conservatism bounds correctly, and we cannot " + \
                           "proceed.")
 
-    priors = conservatism * safe + (1 - conservatism) * dangerous
+    priors_at = priors(conservatism)
 
     try:
-        _, _ = ged.fill_hypercube(hc, standard_k, priors=priors,
-                crash_when_unsolvable=True, cell_range=special_cell_range)
+        # We need to use a deep copy, because this function mutates the
+        # sigma12 entry of the HC!!
+        _, _ = ged.fill_hypercube(cp.deepcopy(hc), standard_k,
+            priors=priors_at, crash_when_unsolvable=True,
+            cell_range=special_cell_range)
     except ValueError:
         print("Conservatism level " + str(conservatism) + " has failed.")
         highest_failed_conservatism = conservatism
--- cassL/quickrun_h_haggle.py
+++ cassL/quickrun_h_haggle.py
@@ -2,16 +2,19 @@ import numpy as np
 from cassL import generate_emu_data as ged
 from cassL import user_interface as ui
 
-standard_k = np.load("data_sets/k/300k.npy")
-lhc_val = np.load("data_sets/Hnu3/lhc_val_initial.npy")
+# If a particular range of cells is already known to cause problems, it may be
+# more efficient to change this value away from None.
+special_cell_range = range(3247, 3250)
+
+standard_k = np.load("data_sets/k/300k.npy", allow_pickle=True)
+hc = np.load("data_sets/Hnu3/lhc_val_initial.npy", allow_pickle=True)
 
 safe = ui.prior_file_to_array("COMET")
 dangerous = ui.prior_file_to_array("CLASSIC")
 
 priors = lambda conservatism: conservatism * safe + (1 - conservatism) * \
          dangerous
-priors_at = priors(0.01)
+priors_at = priors(0.45)
 
-samples_val, rescalers_val = ged.fill_hypercube(lhc_val, standard_k,
-    priors=priors_at, write_period=1000, save_label="Hnu3_val",
-    crash_when_unsolvable=True, cell_range=range(3247, 3250))
+_, _ = ged.fill_hypercube(hc, standard_k, priors=priors_at,
+    crash_when_unsolvable=True, cell_range=special_cell_range)
