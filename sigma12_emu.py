47ec36a80677f463d3823b7c07a203f42de6a60a
3276908917 @ MPELaptop <microwavecow@gmail.com>
2024-01-03 22:55:06 +0100

    Add more intricate pathing
    To the sigma12 script. This should make it easier to process data labeled according to the more traditional scheme for emulators in this project.

--- sigma12_emu.py
+++ sigma12_emu.py
@@ -4,49 +4,54 @@ from cassL import camb_interface as ci
 from cassL import train_emu as te
 import numpy as np
 
+PATH_BASE = "cassL/data_sets/sigma12/"
+
+# Emulator version.
+version = "2"
 priors = ui.prior_file_to_array("MEGA")
 
 def eval_func(cosmology):
     return ci.evaluate_sigma12(cosmology, [1])
     
-checkpoint = 0 # which phase of this script we're on    
+checkpoint = 3 # which phase of this script we're on    
     
 if checkpoint < 1: # skip already-computed stuff
-    sigma12_train_lhs = np.load("lhc_sigma12_train_initial.npy")
+    sigma12_train_lhs = np.load(PATH_BASE + "lhc_train_initial.npy")
 
     sigma12_train_samples, _ = ged.fill_hypercube(sigma12_train_lhs, None,
         priors, eval_func, save_label = "sigma12_train")
 
-    np.save("sigma12_samples_train.npy", sigma12_train_samples)
-    np.save("lhc_sigma12_train_final.npy", sigma12_train_lhs)
+    np.save(PATH_BASE + "samples_train.npy", sigma12_train_samples)
+    np.save(PATH_BASE + "lhc_train_final.npy", sigma12_train_lhs)
 
 if checkpoint < 2:
-    sigma12_test_lhs = np.load("lhc_sigma12_test_initial.npy")
+    sigma12_test_lhs = np.load(PATH_BASE + "lhc_test_initial.npy")
 
     sigma12_test_samples, _ = ged.fill_hypercube(sigma12_test_lhs, None,
         priors, eval_func, save_label = "sigma12_test")
         
-    np.save("sigma12_samples_test.npy", sigma12_test_samples)
-    np.save("lhc_sigma12_test_final.npy", sigma12_test_lhs)
+    np.save(PATH_BASE + "samples_test.npy", sigma12_test_samples)
+    np.save(PATH_BASE + "lhc_test_final.npy", sigma12_test_lhs)
 
 if checkpoint < 3:
-    lhs_train = np.load("lhc_sigma12_train_final.npy")
-    samples_train = np.load("sigma12_samples_train.npy")
+    lhs_train = np.load(PATH_BASE + "lhc_train_final.npy")
+    samples_train = np.load(PATH_BASE + "samples_train.npy")
 
     X_train, Y_train = te.eliminate_unusable_entries(lhs_train, samples_train)
     Y_train2 = Y_train.reshape((len(Y_train), 1))
 
-    trainer = te.Emulator_Trainer("sigma12_v1", X_train, Y_train2, priors,
-                                  False)
+    trainer = te.Emulator_Trainer("sigma12_v" + version, X_train, Y_train2,
+                                  priors, False)
     trainer.train_p_emu()
     trainer.save()
     
 else:
-    trainer = np.load(te.path_to_emus + "sigma12_v1.cle", allow_pickle=True)
+    trainer = np.load(te.path_to_emus + "sigma12_v" + version + ".cle",
+                      allow_pickle=True)
 
 if checkpoint < 4:
-    lhs_test = np.load("lhc_sigma12_test_final.npy")
-    samples_test = np.load("sigma12_samples_test.npy")
+    lhs_test = np.load(PATH_BASE + "lhc_test_final.npy")
+    samples_test = np.load(PATH_BASE + "samples_test.npy")
     X_test, Y_test = te.eliminate_unusable_entries(lhs_test, samples_test)
 
     Y_test2 = Y_test.reshape((len(Y_test), 1))
